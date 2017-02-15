3bd688cfa8ab34411cfcd05d8b8bfedf10bef1c9
katurina <bird.katyrina@gmail.com>
2017-02-15 20:48:00 +0300

    All done

--- src/BinaryTreeUtils.java
+++ src/BinaryTreeUtils.java
@@ -2,11 +2,49 @@
  * Created by Bird on 14.02.2017.
  */
 
-public class BinaryTreeUtils{
-//    public static int size(TreeNode root){
-//
-//    }
-//    public static int height (TreeNode root){}
-//    public static int sum(TreeNode root){}
-//    public static int max (TreeNode root){}
+public class BinaryTreeUtils {
+    public static int left, right, result;
+    public static int n;
+    static int k = 0;
+
+    public static void printTree(TreeNode root) {
+        if (root.left != null) {
+            printTree(root.left);
+        }
+        if (root.right != null) {
+            printTree(root.right);
+        }
+        System.out.print(root.value + " ");
+
+    }
+
+    public static int size(TreeNode root) {
+        if (root.right != null) {
+            size(root.right);
+        }
+        if (root.left != null) {
+            size(root.left);
+        }
+        k++;
+        return k;
+    }
+
+    public static int height(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    public static int sum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return root.value + sum(root.left) + sum(root.right);
+    }
+
+    public static int max(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(root.value, Math.max(root.left.value, root.right.value));
+    }
 }
--- src/Nodes.java
+++ src/Nodes.java
@@ -2,27 +2,41 @@
  * Created by Bird on 14.02.2017.
  */
 public class Nodes {
+    //    public static void main(String[] args) {
+//        Node n = new Node(3, null);
+//        n = new Node(2, n);
+//        n = new Node(1, n);
+//        n = new Node(0, n);
+//
+//        n = SingleLLUtils_2.add(n, 1, 55);
+//
+//
+//        n = SingleLLUtils_2.add(n, 4);
+//        n = SingleLLUtils_2.add(n, 7);
+//        SingleLLUtils_2.printNode(n);
+//
+//        n = SingleLLUtils_2.remove(n);
+//        System.out.println();
+//
+//        SingleLLUtils_2.printNode(n);
+//        System.out.println();
+//        n = SingleLLUtils_2.remove(n, 2);
+//
+//        SingleLLUtils_2.printNode(n);
+//
+//    }
     public static void main(String[] args) {
-        Node n = new Node(3, null);
-        n = new Node(2, n);
-        n = new Node(1, n);
-        n = new Node(0, n);
+        TreeNode n = new TreeNode(1, null, null);
+        n = new TreeNode(2, n, null);
+        n = new TreeNode(3, n, new TreeNode(4, null, null));
+        n = new TreeNode(-1, new TreeNode(5, null, null), n);
 
-        n = SingleLLUtils_2.add(n, 1, 55);
-
-
-        n = SingleLLUtils_2.add(n, 4);
-        n = SingleLLUtils_2.add(n, 7);
-        SingleLLUtils_2.printNode(n);
-
-        n = SingleLLUtils_2.remove(n);
+        BinaryTreeUtils.printTree(n);
         System.out.println();
-
-        SingleLLUtils_2.printNode(n);
-        System.out.println();
-        n = SingleLLUtils_2.remove(n, 2);
-
-        SingleLLUtils_2.printNode(n);
+        System.out.println(BinaryTreeUtils.size(n));
+        System.out.println(BinaryTreeUtils.height(n));
+        System.out.println(BinaryTreeUtils.sum(n));
+        System.out.println(BinaryTreeUtils.max(n));
 
     }
 }
